961e3ee510a9a6745b8300e5c279b5cbcffb2483
Ronan <roleary81@gmail.com>
2022-06-30 01:16:33 +0200

    modify homepage

--- config/element-api.php
+++ config/element-api.php
@@ -17,12 +17,114 @@ return [
                         switch ($block->type->handle) {
                             case 'hero':
                                 $bodyBlocks[] = [
+                                    'uid' => $block->uid,
+                                    'blockType' => 'hero',
                                     'eyebrow' => $block->eyebrow,
+                                    'heading' => $block->heading,
+                                    'subHeadline' => $block->subHeadline
                                 ];
                                 break;
                              case 'header':
                                 $bodyBlocks[] = [
+                                    'uid' => $block->uid,
+                                    'blockType' => 'header',
                                     'headline' => $block->headline,
+                                ];
+                                break;
+                            case 'faq':
+                                $faqRows = [];
+                                foreach($block->faqs->all() as $row){
+                                    $faqRows[] = [
+                                        'question' => $row->question,
+                                        'answer' => $row->answer,
+                                    ];
+                                }
+                                $bodyBlocks[] = [
+                                    'uid' => $block->uid,
+                                    'blockType' => 'faq',
+                                    'faqHeading' => $block->faqHeading,
+                                    'faqLeadtext' => $block->faqLeadtext,
+                                    'faqs' => $faqRows
+                                ];
+                                break;
+                            
+                            // case 'speakers':
+                            //     // $selectedSpeakers = [];
+                            //     $relatedCat = $block->speakerCategory->one()->id;
+                                
+                            //     // $blockSpeakers = Entry::find()
+                            //     //     ->section('speakers')
+                            //     //     ->relatedTo($relatedCat)
+                            //     //     ->limit(10)
+                            //     //     ->all();
+
+                                
+                            //     // $selectedSpeakers = [];
+                            //     //     foreach($blockSpeakers as $spkr){
+                            //     //     $selectedSpeakers[] = [
+                            //     //         'speakerName' => $spkr->speakerName
+                            //     //     ];
+                            //     // }
+
+                            //     $bodyBlocks[] = [
+                            //         'heading' => $block->heading,
+                            //         'speakersIntro' => $block->speakersIntro,
+                            //         // 'speakers' => $selectedSpeakers
+                            //     ];
+                            //     break;
+                            case 'video':
+                                $bodyBlocks[] = [
+                                    'uid' => $block->uid,
+                                    'blockType' => 'video',
+                                    'videoTitle' => $block->videoTitle,
+                                    'videoEmbedCode' => $block->videoEmbedCode,
+                                ];
+                                break;
+                            case 'imageSlider':
+                                $SuperTableRows = [];
+                                foreach ($block->sliderMatrix->all() as $row){
+                                    $SuperTableRows[] = [
+                                        'textSub' => $row->textSub,
+                                        'textHeading' => $row->textHeading,
+                                        'textBackground' => $row->textBackground,
+                                        'slideImage' => $row->slideImage,
+                                        'slideColor' => $row->slideColor->value,
+                                    ];
+                                }
+                                $bodyBlocks[] = [
+                                    'uid' => $block->uid,
+                                    'blockType' => 'imageSlider',
+                                    'sliderTitle' => $block->sliderTitle,
+                                    'sliderMatrix' => $SuperTableRows,
+                                ];
+                                break;
+                            case 'text':
+                                $bodyBlocks[] = [
+                                    'uid' => $block->uid,
+                                    'blockType' => 'text',
+                                    'headline' => $block->headline,
+                                    'articleBody' => $block->articleBody,
+                                ];
+                                break;
+                            case 'textVisual':
+
+                                $TVButtons = [];
+                                foreach ($block->textVisualButtons->all() as $row){
+                                    $TVButtons[] = [
+                                        'linkId' => $row->linkId,
+                                        'linkText' => $row->linkText,
+                                        'linkUrl' => $row->linkUrl,
+                                        'isExternal' => $row->target
+                                    ];
+                                }
+
+                                $bodyBlocks[] = [
+                                    'uid' => $block->uid,
+                                    'blockType' => 'textVisual',
+                                    'title' => $block->textVisualTitle,
+                                    'articleBody' => $block->textVisualContent,
+                                    'image' => $block->textVisualImage,
+                                    'buttons' => $TVButtons,
                                 ];
                                 break;
                         }
@@ -205,8 +307,6 @@ return [
                                         'isExternal' => $row->target
                                     ];
                                 }
-
-
                                 $bodyBlocks[] = [
                                     'uid' => $block->uid,
                                     'blockType' => 'textVisual',
