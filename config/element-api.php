bf10159cffab9350a471fea2a574617195e860db
Ronan <roleary81@gmail.com>
2022-07-02 14:01:26 +0200

    additional groups to test

--- config/element-api.php
+++ config/element-api.php
@@ -150,8 +150,8 @@ return [
 
                     return [
                         'title' => $entry->headline,
-                        'jsonUrl' => UrlHelper::url("api/articles/{$entry->slug}.json"),
-                        // 'summary' => $entry->summary,
+                        'jsonUrl' => UrlHelper::url(":3306/api/articles/{$entry->slug}.json"),
+                    
                     ];
                 },
             ];
@@ -164,14 +164,135 @@ return [
                 'one' => true,
                 'transformer' => function(Entry $entry) {
                     
+                    $bodyBlocks = [];
+                    foreach ($entry->pageBlocks->all() as $block) {
+                        switch ($block->type->handle) {
+                            case 'hero':
+                                $bodyBlocks[] = [
+                                    'blockType' => 'hero',
+                                    'unitId' => $block->unitId,
+                                    'eyebrow' => $block->eyebrow,
+                                    'heading' => $block->heading,
+                                    'subHeading' => $block->subHeading
+                                ];
+                                break;
+                            case 'header':
+                            $bodyBlocks[] = [
+                                'uid' => $block->uid,
+                                'blockType' => 'header',
+                                'headline' => $block->headline,
+                                ];
+                                break;
+                            // case 'faq':
+                            //     $faqRows = [];
+                            //     foreach($block->faqs->all() as $row){
+                            //         $faqRows[] = [
+                            //             'question' => $row->question,
+                            //             'answer' => $row->answer,
+                            //         ];
+                            //     }
+                            //     $bodyBlocks[] = [
+                            //         'uid' => $block->uid,
+                            //         'blockType' => 'faq',
+                            //         'faqHeading' => $block->faqHeading,
+                            //         'faqLeadtext' => $block->faqLeadtext,
+                            //         'faqs' => $faqRows
+                            //     ];
+                            //     break;
+                            
+                            // // case 'speakers':
+                            // //     // $selectedSpeakers = [];
+                            // //     $relatedCat = $block->speakerCategory->one()->id;
+                                
+                            // //     // $blockSpeakers = Entry::find()
+                            // //     //     ->section('speakers')
+                            // //     //     ->relatedTo($relatedCat)
+                            // //     //     ->limit(10)
+                            // //     //     ->all();
+
+                                
+                            // //     // $selectedSpeakers = [];
+                            // //     //     foreach($blockSpeakers as $spkr){
+                            // //     //     $selectedSpeakers[] = [
+                            // //     //         'speakerName' => $spkr->speakerName
+                            // //     //     ];
+                            // //     // }
+
+                            // //     $bodyBlocks[] = [
+                            // //         'heading' => $block->heading,
+                            // //         'speakersIntro' => $block->speakersIntro,
+                            // //         // 'speakers' => $selectedSpeakers
+                            // //     ];
+                            // //     break;
+                            // case 'video':
+                            //     $bodyBlocks[] = [
+                            //         'uid' => $block->uid,
+                            //         'blockType' => 'video',
+                            //         'videoTitle' => $block->videoTitle,
+                            //         'videoEmbedCode' => $block->videoEmbedCode,
+                            //     ];
+                            //     break;
+                            // case 'imageSlider':
+                            //     $SuperTableRows = [];
+                            //     foreach ($block->sliderMatrix->all() as $row){
+                            //         $SuperTableRows[] = [
+                            //             'textSub' => $row->textSub,
+                            //             'textHeading' => $row->textHeading,
+                            //             'textBackground' => $row->textBackground,
+                            //             'slideImage' => $row->slideImage,
+                            //             'slideColor' => $row->slideColor->value,
+                            //         ];
+                            //     }
+                            //     $bodyBlocks[] = [
+                            //         'uid' => $block->uid,
+                            //         'blockType' => 'imageSlider',
+                            //         'sliderTitle' => $block->sliderTitle,
+                            //         'sliderMatrix' => $SuperTableRows,
+                            //     ];
+                            //     break;
+                            // case 'text':
+                            //     $bodyBlocks[] = [
+                            //         'uid' => $block->uid,
+                            //         'blockType' => 'text',
+                            //         'headline' => $block->headline,
+                            //         'articleBody' => $block->articleBody,
+                            //     ];
+                            //     break;
+                            // case 'textVisual':
+
+                            //     $TVButtons = [];
+                            //     foreach ($block->textVisualButtons->all() as $row){
+                            //         $TVButtons[] = [
+                            //             'linkId' => $row->linkId,
+                            //             'linkText' => $row->linkText,
+                            //             'linkUrl' => $row->linkUrl,
+                            //             'isExternal' => $row->target
+                            //         ];
+                            //     }
+
+                            //     $bodyBlocks[] = [
+                            //         'uid' => $block->uid,
+                            //         'blockType' => 'textVisual',
+                            //         'title' => $block->textVisualTitle,
+                            //         'articleBody' => $block->textVisualContent,
+                            //         'image' => $block->textVisualImage,
+                            //         'buttons' => $TVButtons,
+                            //     ];
+                            //     break;
+                        }
+                    }
+
                     return [
                         'title' => $entry->title,
                         'headline' => $entry->headline,
                         'subHeadline' => $entry->subHeadline,
-                        'excerpt' => $entry->excerpt,
-                        'format' => $entry->articleFormat,
+                        'boo' => 'bees',
+                        // 'excerpt' => $entry->excerpt,
+                        // 'format' => $entry->articleFormat,
                         'body' => $entry->articleBody,
-                        'jsonUrl' => UrlHelper::url("api/articles/{$entry->slug}.json"),
+                        'related' => $entry->manualRelatedArticles->all(),
+                        // 'blocks' => $bodyBlocks,
+                        'jsonUrl' => UrlHelper::url(":3306/api/articles/{$entry->slug}.json"),
                        
                     ];
                 },
